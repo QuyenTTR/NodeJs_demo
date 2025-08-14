c1e55fedfe4c02deb6a513bd540e7671b8f4cf6b
Ho Luc Quyen <holucquyen04@gmail.com>
2025-08-15 00:11:37 +0700

    Them gitignore

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 app.use(morgan('combined'));
 
 app.engine('hbs', handlebars.engine({
@@ -14,6 +17,8 @@ app.engine('hbs', handlebars.engine({
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/resources/views');
+
+
 
 app.get('/hello-world', (req, res) => {
     res.send('Hello World!');
@@ -23,9 +28,15 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+
 app.get('/news', (req, res) => {
     res.render('news');
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
